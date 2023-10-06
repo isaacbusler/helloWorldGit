3cb5d76b5f98cf183d0b1454efd823270520142b
JainaHersh <jainah@outlook.com>
2023-10-06 13:30:00 -0400

    updated to greet user by name

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scnr = new Scanner(System.in);
+        String name;
+
+        System.out.println("Enter name: ");
+        name = scnr.next();
+        System.out.println("Hello " + name);
     }
 }
